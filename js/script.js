3a6ae708cbc628e4061717229d082affa97b0de1
IDELACRUZB <economista.isac@gmail.com>
2026-01-22 23:42:01 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const img = ["vida.jpg", "vida2.jpg", "vida3.jpg", "vida4.jpg", "vida5.jpg", "vida6.jpg","vida7.jpeg", 'vida8.jpg','vida9.jpg', "vida10.jpeg", ];
+  const veces = 1000;
+  const imagenes = Array(veces).fill(img).flat();
+
+  imagenes.forEach(imagen => {
+    const img = document.createElement("img");
+    img.src = `imagenes/${imagen}`;
+    grid.appendChild(img);
+  });
+});
+
+function playAudio() {
+    document.getElementById("miAudio").play();
+  }
