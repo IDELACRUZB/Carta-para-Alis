82ab979ead8c959e021bc8dcfa246492f508d4a6
IDELACRUZB <economista.isac@gmail.com>
2026-05-02 20:20:59 -0500

    Actualizaciones 02-05-2026

--- js/script_carta.js
+++ js/script_carta.js
@@ -14,3 +14,44 @@ document.addEventListener("DOMContentLoaded", () => {
 function playAudio() {
       document.getElementById("miAudio").play();
   }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const img = ["vida.jpg", "vida2.jpg", "vida3.jpg", "vida4.jpg", "vida5.jpg", "vida6.jpg","vida7.jpeg", 'vida8.jpg', 'vida9.jpg', "vida10.jpeg", ];
+  const veces = 1000;
+  const imagenes = Array(veces).fill(img).flat();
+
+  imagenes.forEach(imagen => {
+    const img = document.createElement("img");
+    img.src = `../imagenes/${imagen}`;
+    grid.appendChild(img);
+  });
+});
+
+function playAudio() {
+      document.getElementById("miAudio").play();
+  }
+
+const audio = document.getElementById("miAudio");
+const btn = document.getElementById("audioBtn");
+
+// Intentar reproducir al cargar
+window.addEventListener("load", () => {
+  audio.play().then(() => {
+    btn.textContent = "Pausar Música";
+  }).catch(() => {
+    // Si el navegador bloquea autoplay
+    btn.textContent = "Reproducir Música";
+  });
+});
+
+// Función del botón
+function toggleAudio() {
+  if (audio.paused) {
+    audio.play();
+    btn.textContent = "Pausar Música";
+  } else {
+    audio.pause();
+    btn.textContent = "Reproducir Música";
+  }
+}
